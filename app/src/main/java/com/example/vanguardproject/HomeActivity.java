f3f953d7f82356f88bdccefebc0af378f0189146
edmar <edmar_pelayo@yahoo.com>
2022-05-03 17:49:17 -0400

    New Stuff

--- app/src/main/java/com/example/vanguardproject/HomeActivity.java
+++ app/src/main/java/com/example/vanguardproject/HomeActivity.java
@@ -3,12 +3,39 @@ package com.example.vanguardproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
-
+    ProgressBar pb;
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        prog();
     }
+
+    public void prog(){
+        pb = (ProgressBar)findViewById(R.id.pb);
+        final Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                pb.setProgress(counter);
+                if(counter==65){
+                    t.cancel();
+                }
+            }
+        };
+        t.schedule(tt,0,50);
+    }
+
 }
